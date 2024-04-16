c6752e49f2053d55d00fe85fbd1f884eb92646b5
Vinay_Sharma <vinaysharma31.12.1999@gmail.com>
2024-04-17 00:02:41 +0530

    done basics

--- lib/src/card_view.dart
+++ lib/src/card_view.dart
@@ -1,4 +1,4 @@
-import 'dart:developer';
+import 'dart:ffi';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -15,10 +15,17 @@ class CardView extends StatefulWidget {
   State<CardView> createState() => _CardViewState();
 }
 
-class _CardViewState extends State<CardView>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController animationController;
+class _CardViewState extends State<CardView> with TickerProviderStateMixin {
+  late final AnimationController initialAnimationController;
+  late final AnimationController cardAnimationController;
+  late final AnimationController viewAnimationController;
+
   late final Animation<double> initialAnimation;
+  late final Animation<double> cardTranslateAnimation;
+  late final Animation<double> cardScaleUpAnimation;
+  late final Animation<double> cardScaleDownAnimation;
+  late final Animation<double> cardOpacityAnimation;
+  late final Animation<double> viewChangeAnimation;
 
   final int maxCardLen = 3;
   int currentIndex = 0;
@@ -38,54 +45,107 @@ class _CardViewState extends State<CardView>
     allImages = widget.images;
     viewImages = [allImages[0]];
     cardImages = [...allImages]..removeAt(0);
-    // cardImages = cardImages.reversed.toList();
   }
 
   void setupAnimation() {
-    animationController = AnimationController(
+    initialAnimationController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 500),
     );
-    initialAnimation = Tween<double>(begin: 0, end: 20).animate(
-      CurvedAnimation(parent: animationController, curve: Curves.decelerate),
+    cardAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
     );
-    Future.microtask(() => animationController.forward());
+    viewAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    initialAnimation = Tween<double>(begin: 0, end: 20).animate(
+      CurvedAnimation(
+        parent: initialAnimationController,
+        curve: Curves.decelerate,
+      ),
+    );
+    cardTranslateAnimation = Tween<double>(begin: 0, end: 50).animate(
+      CurvedAnimation(
+        parent: cardAnimationController,
+        curve: const Interval(0, 0.4, curve: Curves.easeIn),
+      ),
+    );
+    cardScaleUpAnimation = Tween<double>(begin: 1, end: 1.5).animate(
+      CurvedAnimation(
+        parent: cardAnimationController,
+        curve: const Interval(0.45, 0.65, curve: Curves.bounceOut),
+      ),
+    );
+    cardScaleDownAnimation = Tween<double>(begin: 0, end: 1.5).animate(
+      CurvedAnimation(
+        parent: cardAnimationController,
+        curve: const Interval(0.7, 0.9, curve: Curves.bounceIn),
+      ),
+    );
+    cardOpacityAnimation = Tween<double>(begin: 1, end: 0).animate(
+      CurvedAnimation(
+        parent: cardAnimationController,
+        curve: const Interval(0.8, 1),
+      ),
+    );
+    viewChangeAnimation = Tween<double>(begin: 0.4, end: 1).animate(
+      CurvedAnimation(
+        parent: viewAnimationController,
+        curve: Curves.easeIn,
+        reverseCurve: Curves.easeOut,
+      ),
+    );
+    Future.microtask(() {
+      initialAnimationController.forward();
+      viewAnimationController.forward();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onHorizontalDragEnd: (details) {
+      onHorizontalDragEnd: (details) async {
         final dx = details.primaryVelocity ?? 0;
         final leftSwipe = dx < 0;
 
         if (leftSwipe) {
           if (cardImages.isEmpty) return;
+          await cardAnimationController.forward();
           final data = cardImages.removeAt(0);
           viewImages.add(data);
           currentIndex = currentIndex + 1;
+          cardAnimationController.reset();
+          viewAnimationController.reset();
+          viewAnimationController.forward();
         } else {
-          if (viewImages.length == 1) return;
-          final data = viewImages.removeAt(currentIndex);
-          cardImages.insert(0, data);
+          if (viewImages.length == 1) {
+            initialAnimationController.reset();
+            initialAnimationController.forward();
+            return;
+          }
+          await viewAnimationController.reverse();
           currentIndex = currentIndex - 1;
+          viewAnimationController.forward(from: 1.0);
+          final topViewIndex = viewImages.length - 1;
+          final data = viewImages.removeAt(topViewIndex);
+          cardImages.insert(0, data);
+          await cardAnimationController.reverse();
+          cardAnimationController.reset();
         }
+
         setState(() {});
-        animationController.forward();
       },
       child: SafeArea(
         child: Stack(
           children: [
-            Positioned.fill(
-              child: CachedNetworkImage(
-                imageUrl: viewImages[currentIndex],
-              ),
-            ),
+            for (int i = 0; i < viewImages.length; i++) fullView(i),
             Positioned(
               height: 200,
               width: 300,
               bottom: 0,
-              right: 0,
+              right: 10,
               child: smallPreviewCard(),
             )
           ],
@@ -94,11 +154,40 @@ class _CardViewState extends State<CardView>
     );
   }
 
+  Widget fullView(int index) {
+    if (currentIndex == index) {
+      final isLast = index == 0;
+      return AnimatedBuilder(
+        animation: viewChangeAnimation,
+        builder: (context, child) {
+          return Positioned.fill(
+            child: Transform.scale(
+              origin: const Offset(0, -50),
+              alignment: isLast ? Alignment.center : Alignment.bottomCenter,
+              scale: viewChangeAnimation.value,
+              child: CachedNetworkImage(
+                imageUrl: viewImages[index],
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      );
+    }
+
+    return Positioned.fill(
+      child: CachedNetworkImage(
+        imageUrl: viewImages[index],
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
   Widget smallPreviewCard() {
     final bool moreItems = cardImages.length >= maxCardLen;
     final int initial = (moreItems ? maxCardLen : cardImages.length) - 1;
     return AnimatedBuilder(
-      animation: initialAnimation,
+      animation: initialAnimationController,
       builder: (context, child) {
         return Stack(
           alignment: Alignment.centerLeft,
@@ -112,21 +201,47 @@ class _CardViewState extends State<CardView>
 
   Widget miniCard({required int imageIndex}) {
     final pos = maxCardLen - imageIndex;
+
+    if (imageIndex == 0) {
+      return AnimatedBuilder(
+        animation: cardAnimationController,
+        builder: (context, child) {
+          final rightInitial = initialAnimation.value * pos;
+          final scaleCard =
+              cardScaleUpAnimation.value - cardScaleDownAnimation.value;
+          return Positioned(
+            right: rightInitial + cardTranslateAnimation.value,
+            child: Opacity(
+              opacity: cardOpacityAnimation.value,
+              child: Transform.scale(
+                scale: scaleCard,
+                child: cardWidget(pos, imageIndex),
+              ),
+            ),
+          );
+        },
+      );
+    }
+
     return Positioned(
       right: initialAnimation.value * pos,
-      child: SizedBox(
-        height: 100.0 + 20 * pos,
-        width: 105,
-        child: Card(
-          elevation: 5.0 * pos,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          clipBehavior: Clip.hardEdge,
-          child: CachedNetworkImage(
-            imageUrl: cardImages[imageIndex],
-            fit: BoxFit.cover,
-          ),
+      child: cardWidget(pos, imageIndex),
+    );
+  }
+
+  SizedBox cardWidget(int pos, int imageIndex) {
+    return SizedBox(
+      height: 100.0 + 20 * pos,
+      width: 105,
+      child: Card(
+        elevation: 5.0 * pos,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        clipBehavior: Clip.hardEdge,
+        child: CachedNetworkImage(
+          imageUrl: cardImages[imageIndex],
+          fit: BoxFit.cover,
         ),
       ),
     );
@@ -134,7 +249,8 @@ class _CardViewState extends State<CardView>
 
   @override
   void dispose() {
-    animationController.dispose();
+    initialAnimationController.dispose();
+    cardAnimationController.dispose();
     super.dispose();
   }
 }
